1f2203f567a2851c265b73e38f5f70c2ac43200d
G90DD <georgek1990bb@gmail.com>
2018-06-16 21:37:03 +0300

    Login view -> index
    changed the login view to be the default one, opened by index

--- urls.py
+++ urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),  #auto pou kanei to name=index einai na dinei ena onoma sto mapping, prokeimenou na anaferomaste se auto allou ston kodika-reverse url mapping
+    path('', auth_views.login, name='login'),  #auto pou kanei to name=index einai na dinei ena onoma sto mapping, prokeimenou na anaferomaste se auto allou ston kodika-reverse url mapping
     path('profile/', views.profile, name='profile'),
     path('front_page/', views.front_page, name='front_page'),
     path('groups/', views.groups, name='groups'),
