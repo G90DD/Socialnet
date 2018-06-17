78a77b860021e48052dfa8459697b0414baf0237
G90DD <georgek1990bb@gmail.com>
2018-06-18 00:39:35 +0300

    css changes
    css on login screen

--- urls.py
+++ urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
 
 from . import views
 
